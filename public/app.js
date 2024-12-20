851f4d9013ce395d976b104685ecf9203167cb73
efr-en <efrenlaris@gmail.com>
2024-12-19 21:03:43 -0500

    Product card like structure added for better movie organization, movieDiv changed to movieCard and added more CSS

--- public/app.js
+++ public/app.js
@@ -8,25 +8,26 @@ document.getElementById('movieForm').addEventListener('submit', async (e) => { /
 
     if (data.similarMovies && data.similarMovies.length > 0) {
         data.similarMovies.forEach(movie => {
-        const movieDiv = document.createElement('div'); // Shows the movie title and release date when found
-        movieDiv.style.marginBottom = '20px';
+        const movieCard = document.createElement('div'); // Shows the movie title and release date when found
+        movieCard.className = 'movie-card';
+        // movieDiv.style.marginBottom = '20px';
 
         if (movie.poster_path) { // If movie has an image display it
             const poster = document.createElement('img');
             poster.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
             poster.alt = `${movie.title} Poster`; // error case for no image 
             poster.style.width = '200px';
-            movieDiv.appendChild(poster);
+            movieCard.appendChild(poster);
         }
 
         const title = document.createElement('h3');
         title.textContent = `${movie.title} (Release Date: ${movie.release_date})`;
-        movieDiv.appendChild(title);
+        movieCard.appendChild(title);
 
         const description = document.createElement('p'); // Creates paragraph text for movie desc.
         description.textContent = movie.overview || 'No description available.'; // error case
-        movieDiv.appendChild(description);
-        resultsDiv.appendChild(movieDiv);
+        movieCard.appendChild(description);
+        resultsDiv.appendChild(movieCard);
         });
 
     } else {
