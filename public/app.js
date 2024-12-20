fa5339ccb3f0dd512ebc59cca4f9b4eabfb2a862
efr-en <efrenlaris@gmail.com>
2024-12-19 21:54:28 -0500

    Added notes to functionality of code and CSS font

--- public/app.js
+++ public/app.js
@@ -2,19 +2,18 @@ document.getElementById('movieForm').addEventListener('submit', async (e) => { /
     e.preventDefault();
     const movieName = document.getElementById('movieName').value; // Retrieves the value entered into the input field w/movieName ID
     const response = await fetch(`/similar?movieName=${encodeURIComponent(movieName)}`);
-    const data = await response.json();
+    const data = await response.json(); // To parse the server's response as JSON and store it into data
     const resultsDiv = document.getElementById('results'); //Attaches to the results section in html doc to display results
     resultsDiv.innerHTML = '';
 
     if (data.similarMovies && data.similarMovies.length > 0) {
-        data.similarMovies.forEach(movie => {
+        data.similarMovies.forEach(movie => { // Loops through each movie to create a movie card for each
         const movieCard = document.createElement('div'); // Shows the movie title and release date when found
         movieCard.className = 'movie-card';
-        // movieDiv.style.marginBottom = '20px';
 
         if (movie.poster_path) { // If movie has an image display it
             const poster = document.createElement('img');
-            poster.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+            poster.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`; // Image source link from TMDB
             poster.alt = `${movie.title} Poster`; // error case for no image 
             poster.style.width = '200px';
             movieCard.appendChild(poster);
